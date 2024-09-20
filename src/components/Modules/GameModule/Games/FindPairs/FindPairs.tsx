fcafeda9acf0920104e6c3deba8c912f4ac55e30
Кирилл Зайцев <kirill@Kirills-MacBook-Air.local>
2024-09-20 16:46:41 +0500

    hotfix: fixed UI Find-pairs game and logic

--- src/components/Modules/GameModule/Games/FindPairs/FindPairs.tsx
+++ src/components/Modules/GameModule/Games/FindPairs/FindPairs.tsx
@@ -33,83 +33,130 @@ const FindPairs: React.FC<FindPairsProps> = ({
 }) => {
   const [shuffledFields, setShuffledFields] = useState<Field[]>([]);
   const [flippedCards, setFlippedCards] = useState<number[]>([]);
-  const [selectedPair, setSelectedPair] = useState<number | null>(null);
   const [matches, setMatches] = useState<number[]>([]);
+  const [correctCount, setCorrectCount] = useState(0); // Счетчик правильных карточек для текущего вопроса
+  const [incorrectCount, setIncorrectCount] = useState(0); // Счетчик неправильных карточек для текущего вопроса
+  const [globalCorrectCount, setGlobalCorrectCount] = useState(0); // Глобальный счетчик правильных карточек
   const [isResultsChecked, setIsResultsChecked] = useState(false);
+  const [initialReveal, setInitialReveal] = useState(true); // Состояние для начального показа карт
+  const [clickBlocked, setClickBlocked] = useState(true); // Блокировка кликов на начальной стадии
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0); // Индекс текущего вопроса
 
   useEffect(() => {
     if (questions.length > 0) {
-      setShuffledFields(shuffleArray(questions[0].fields));
+      setShuffledFields(shuffleArray(questions[currentQuestionIndex].fields));
     }
-  }, [questions]);
+
+    setTimeout(() => {
+      setInitialReveal(false);
+      setClickBlocked(false);
+    }, 1500);
+    
+    // Если это последний вопрос, по завершению обновляем setIsPlayingUser
+    if (isResultsChecked && currentQuestionIndex + 1 === questions.length) {
+      setTimeout(() => {
+        setIsEnd(true);
+        setIsPlayingUser({
+          ...isPlayingUser,
+          correct:  incorrectCount === 0 ? globalCorrectCount : globalCorrectCount / incorrectCount,
+          incorrect: incorrectCount,
+          currentTime: currentTime,
+        });
+      }, 2000);      
+    }
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [questions, currentQuestionIndex, isResultsChecked]);
 
   const handleCardClick = (id: number) => {
-    // Если карта уже перевёрнута, игнорируем клик
-    if (flippedCards.includes(id)) return;
+    if (clickBlocked || isResultsChecked) return; // Запрещаем действия после отправки результатов
 
-    if (selectedPair === null) {
-      // Первая карта выбирается
-      setSelectedPair(id);
+    if (flippedCards.includes(id)) {
+      // Если карточка уже открыта, закрываем ее
+      setFlippedCards(flippedCards.filter(cardId => cardId !== id));
     } else {
-      // Вторая карта выбирается
-      const firstCard = shuffledFields.find(field => field.id === selectedPair);
-      const secondCard = shuffledFields.find(field => field.id === id);
+      // Если карточка закрыта, открываем ее
+      setFlippedCards([...flippedCards, id]);
 
-      if (firstCard && secondCard && firstCard.isCorrect === secondCard.isCorrect) {
-        // Если пара совпала
-        setFlippedCards([...flippedCards, selectedPair, id]);
-        setMatches([...matches, selectedPair, id]); // Добавляем в список совпадений
+      // Проверяем, все ли правильные карточки открыты
+      const correctCards = shuffledFields.filter(field => field.isCorrect).map(field => field.id);
+      const selectedCorrect = [...flippedCards, id].filter(cardId => correctCards.includes(cardId));
+
+      // Если все правильные карты выбраны, добавляем их в список совпадений
+      if (selectedCorrect.length === correctCards.length) {
+        setMatches([...matches, ...correctCards]);
       }
-
-      // Сброс выбора
-      setSelectedPair(null);
     }
   };
 
   const checkResults = () => {
-    setIsResultsChecked(true);
+    let correct = 0;
+    let incorrect = 0;
+
+    flippedCards.forEach(cardId => {
+      const field = shuffledFields.find(field => field.id === cardId);
+      if (field?.isCorrect) {
+        correct++;
+      } else {
+        incorrect++;
+      }
+    });
+
+    setCorrectCount(correct);   // Счетчик правильных карточек для текущего вопроса
+    setIncorrectCount(incorrect); // Счетчик неправильных карточек для текущего вопроса
+    setGlobalCorrectCount(globalCorrectCount + correct); // Обновляем глобальный счетчик правильных карточек
+    setIsResultsChecked(true);   // Подтверждаем, что результат проверен
+  };
+
+  const handleNextQuestion = () => {
+    setIsResultsChecked(false); 
+    setFlippedCards([]); 
+    setMatches([]);
+    setCorrectCount(0); 
+    setIncorrectCount(0); 
+    setInitialReveal(true); 
+    setClickBlocked(true); 
+
+    if (currentQuestionIndex + 1 < questions.length) {
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+    }
   };
 
   return (
     <div className={m.container}>
-      {/* Отображаем текст для пары */}
-      <h1>{questions[0].pairName}</h1>
+      <div className={m.titleWraper}>
+        <span className={m.span}>Найдите подходящию пару к слову: </span>
+        <h1 className={m.title}>{questions[currentQuestionIndex].pairName}</h1>
+      </div>
 
       <div className={m.cards}>
         {shuffledFields.map((field) => (
           <div
             key={field.id}
-            className={`${m.card} ${flippedCards.includes(field.id) ? m.flipped : ''}`}
+            className={`${m.card} ${flippedCards.includes(field.id) || initialReveal ? m.flipped : ''} 
+              ${isResultsChecked && flippedCards.includes(field.id) && field.isCorrect ? m.correctShadow : ''} 
+              ${isResultsChecked && flippedCards.includes(field.id) && !field.isCorrect ? m.incorrectShadow : ''}`}
             onClick={() => handleCardClick(field.id)}
           >
             <div className={m.cardInner}>
-              {/* Передняя сторона карты */}
-              <div className={m.cardFront}>
-                {/* Это будет закрытая плитка */}
-                <div className={m.tile}></div>
-              </div>
-              {/* Задняя сторона карты */}
-              <div className={m.cardBack}>
-                {field.answer}
-              </div>
+              <div className={m.cardFront} />
+              <div className={m.cardBack}>{field.answer}</div>
             </div>
           </div>
         ))}
       </div>
-
-      {/* Кнопка для проверки результатов */}
-      <button className={m.checkButton} onClick={checkResults}>
-        Проверить результаты
-      </button>
-
-      {/* Отображение результатов */}
-      {isResultsChecked && (
-        <div className={m.results}>
-          {matches.length === shuffledFields.length
-            ? 'Поздравляем! Вы нашли все пары!'
-            : `Вы нашли ${matches.length / 2} из ${shuffledFields.length / 2} пар.`}
-        </div>
-      )}
+      
+      <div className={m.buttons}>
+        {isResultsChecked && currentQuestionIndex + 1 < questions.length ? (
+          <button className={m.button} onClick={handleNextQuestion}>
+            Продолжить
+          </button>
+        ) : (
+          <button className={flippedCards.length !== 0 ? m.button : m.disabled} onClick={checkResults} disabled={flippedCards.length === 0}>
+            Отправить результат
+          </button>
+        )}
+      </div>
     </div>
   );
 };
